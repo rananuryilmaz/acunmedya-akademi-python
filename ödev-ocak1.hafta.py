4a25a90739536f013adac2f3cc073709caa660ad
Rana Yılmaz <yilmazrananur37@gmail.com>
2025-03-15 02:10:22 +0300

    Add files via upload

---
+++ ödev-ocak1.hafta.py
@@ -0,0 +1,22 @@
+isim = input("Adınızı giriniz: ")
+yaş = int(input("Yaşınızı giriniz: "))
+doğum_yılı = int(input("Doğum yılınızı giriniz: "))
+
+print(f"Merhaba {isim}:) {yaş} yaşındasın ve {doğum_yılı} yılında doğmuşsun.")
+
+
+print('*******************')
+
+sayi1 = float(input("1. sayıyı giriniz: "))
+sayi2 = float(input("2. sayıyı giriniz: "))
+
+Toplam = sayi1 + sayi2
+Fark = sayi1 - sayi2
+Çarpım = sayi1 * sayi2
+Bölüm = sayi1 / sayi2
+
+print(f"TOPLAMA: {sayi1} + {sayi2} =  {Toplam}")
+print(f"FARK   :{sayi1} - {sayi2}  =  {Fark}")
+print(f"ÇARPMA :{sayi1} * {sayi2}  =  {Çarpım}")
+print(f"BÖLME  :{sayi1} / {sayi2}  =  {Bölüm}")
+
